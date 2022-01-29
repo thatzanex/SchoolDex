62950c0eb1b9445cf84c900fca745728b95f2b8d
mo0864 <97745609+mo0864@users.noreply.github.com>
2022-01-29 13:51:01 +0100

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -30,7 +30,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   static const List<Widget> _widgetOptions = <Widget>[
     Text(
       'Home',
-      style: optionStyle,
+      style: TextStyle(fontStyle: FontStyle.italic),
     ),
     Text(
       'Business',
