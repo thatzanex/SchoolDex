c14b68b6c4e2e07be9ea7d166792d39f160e5d9c
mo0864 <97745609+mo0864@users.noreply.github.com>
2022-04-28 18:51:45 +0200

    Nachhillfe als Map

--- schooldexV1/lib/models/nachhilfe.dart
+++ schooldexV1/lib/models/nachhilfe.dart
@@ -12,4 +12,14 @@ class Nachhilfe {
     this.beschreibung,
     required this.colorcard,
   });
+
+  factory Nachhilfe.fromJson(Map<String, dynamic> json) {
+    return Nachhilfe(
+      id: json['id'] as int,
+      fach: json['fach'] as String,
+      jahrgang: json['jahrgang'] as String,
+      beschreibung: json['beschreibung'] as String,
+      colorcard: json['colorcard'] as int,
+    );
+  }
 }
