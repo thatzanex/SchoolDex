9e5858cfceb81ea9c24b53a6aa46218869772a0c
mo0864 <97745609+mo0864@users.noreply.github.com>
2022-05-21 00:08:05 +0200

    Erster Schritt zu Lokales merken

--- schooldexV1/lib/models/account.dart
+++ schooldexV1/lib/models/account.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 class Account {
   String? id;
   String benutzername;
@@ -13,6 +15,14 @@ class Account {
     required this.status,
   });
 
+  String toJson() => json.encode(toMapLocal());
+
+  factory Account.fromJson(String source) =>
+      Account.fromMap(json.decode(source));
+
+  String toStringLocal() =>
+      'ShoppingItem(id: $id, benutzername: $benutzername, passwort: $passwort, schulname: $schulname status: $status)';
+
   factory Account.fromMap(Map<String, dynamic> map) {
     return Account(
       id: map['id'] as String,
@@ -20,5 +30,25 @@ class Account {
       passwort: map['passwort'] as String,
       status: map['status'] as String,
     );
+  }
+
+  factory Account.fromMapLocal(Map<String, dynamic> map) {
+    return Account(
+      id: map['id'] as String,
+      benutzername: map['benutzername'] as String,
+      passwort: map['passwort'] as String,
+      schulname: map['schulname'] as String,
+      status: map['status'] as String,
+    );
+  }
+
+  Map<String, dynamic> toMapLocal() {
+    return {
+      'id': id,
+      'benutzername': benutzername,
+      'passwort': passwort,
+      'schulname': schulname,
+      'status': status,
+    };
   }
 }
